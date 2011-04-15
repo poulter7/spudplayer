04ee5da741d7b5968e837748a09ad66a77d5e131
Jonathan Poulter <poulter7@gmail.com>
2011-04-15 05:48:47 +0100

    speed increases

--- src_cust/shef/strategies/uct/UCTNeuralStrategy.java
+++ src_cust/shef/strategies/uct/UCTNeuralStrategy.java
@@ -18,39 +18,38 @@ import util.statemachine.exceptions.TransitionDefinitionException;
 public class UCTNeuralStrategy extends UCTGamer {
 
 	private CIL2PManager cil2pManager;
-	protected static final float C = 50;
 
 	/**
 	 * Setup the UCT game tree
 	 */
 	@Override
-	public void stateMachineMetaGame(long timeout) throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
+	public void stateMachineMetaGame(final long timeout) throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
 		System.out.println("init: " + timeout);
 		CIL2PNet net = CIL2PFactory.modeNetFromGame(getMatch().getGame());
 		cil2pManager = new CIL2PManager(net);
 		super.stateMachineMetaGame(timeout);
 	}
 
-	protected List<Double> completeRollout(MachineState from, int fromLvl) throws MoveDefinitionException, TransitionDefinitionException, GoalDefinitionException {
+	protected List<Double> completeRollout(final MachineState from, final int fromLvl) throws MoveDefinitionException, TransitionDefinitionException, GoalDefinitionException {
 		int simDepth = fromLvl;
 		int levelPlayer = (simDepth % roleCount);
-		//        
+		
 		MachineState current = from;
 		do { // play the best move for the current player
 
-			List<MachineState> nextStats = theMachine.getNextStates(current);
-			int nextStateCount = nextStats.size();
+			List<MachineState> nextStates = theMachine.getNextStates(current);
+			int nextStateCount = nextStates.size();
 
 			double bestChildScoreGAUSS = 0;
 			int bestChildIndexGAUSS = 0;
 			for (int i = 0; i < nextStateCount; i++) {
-				double childScoreGAUSS = cil2pManager.getStateValueGaussian(nextStats.get(i), levelPlayer);
+				double childScoreGAUSS = cil2pManager.getStateValueGaussian(nextStates.get(i), levelPlayer);
 				if (childScoreGAUSS > bestChildScoreGAUSS) {
 					bestChildScoreGAUSS = childScoreGAUSS;
 					bestChildIndexGAUSS = i;
 				}
 			}
-			current = nextStats.get(bestChildIndexGAUSS);
+			current = nextStates.get(bestChildIndexGAUSS);
 			simDepth++;
 
 			levelPlayer = (simDepth % roleCount);
